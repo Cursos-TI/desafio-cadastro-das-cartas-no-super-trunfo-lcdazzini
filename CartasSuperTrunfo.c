3d6f6b10befa9323316bebc437b162269afc71ab
Luiz C Dazzini <lcdazzini@users.noreply.github.com>
2025-01-22 21:05:04 +0000

    Definição de novos métodos. Criação de struct.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
+typedef struct {
+    char codigo[6];
+    char nome[100];
+    int qtdPontosTuristicos;
+    float area;
+    float pib;
+    int populacao;
+    float pibPerCapita;
+    float densidadePopulacional;
+} Carta;
+
 float calculaPibPerCapita(float pib, int populacao) {
     return pib / populacao;
 }
 
 float calculaDensidadePopulacional(float area, int populacao) {
+    if (area == 0) {
+        return 0;
+    }
     return populacao / area;
 }
 
-int main() {
-    int qtdPontosTuristicos, populacao;
-    float area, pib, pibPerCapita, densidadePopulacional;
-    char nomeCidade[100], codigoCidade[5];
-
-    // INÍCIO Cabeçalho de instruções
+void exibirInstrucoes() {
     printf("Bem vindo ao cadastro de cartas do jogo super trunfo!!\n");
     printf("Para facilitar a interacao, faremos a inclusao dos dados das cartas um por um, sendo elas: \n");
     printf("1 - Codigo Cidade. Para o cadastro desse campo deve ser inserido um valor texto nos seguintes parâmetros, não superando a quantidade de 5 caracteres: [Letra][Numero]. \n");
@@ -26,42 +35,51 @@ int main() {
     printf("Ex.: Desejo inserir o valor 3214,45. Esse valor deve ser inserido da seguinte forma: '3214.45'. Onde a virgula foi substituido por ponto. \n");
     printf("6 - População. Para o cadastro desse campo deve ser inserido um valor inteiro. \n");
     printf("\nVamos seguir com o cadastro da carta. Após fazer a inclusao, pressione a tela ENTER:\n");
-    // FIM Cabeçalho de instruções
-    
-    // Início inclusão de campos
+}
+
+// Função para capturar os dados de uma cidade
+void capturarDados(Carta* carta) {
     printf("1 - Digite o CODIGO da cidade: ");
-    scanf("%s", codigoCidade);
+    scanf("%s", carta->codigo);
     
     printf("2 - Digite o NOME da cidade: ");
-    scanf("%s", nomeCidade);
+    scanf("%s", carta->nome);
 
     printf("3 - Digite a QUANTIDADE DE PONTOS TURISTICOS da cidade: ");
-    scanf("%d", &qtdPontosTuristicos);
+    scanf("%d", &carta->qtdPontosTuristicos);
 
     printf("4 - Digite a AREA da cidade: ");
-    scanf("%f", &area);
+    scanf("%f", &carta->area);
 
     printf("5 - Digite o PIB da cidade: ");
-    scanf("%f", &pib);
+    scanf("%f", &carta->pib);
 
     printf("6 - Digite a POPULACAO da cidade: ");
-    scanf("%d", &populacao);
-    // FIM inclusão de campos
+    scanf("%d", &carta->populacao);
+}
 
-    pibPerCapita = calculaPibPerCapita(pib, populacao);
-    densidadePopulacional = calculaDensidadePopulacional(area, populacao);
+void exibirResultados(Carta* carta) {
+    carta->pibPerCapita = calculaPibPerCapita(carta->pib, carta->populacao);
+    carta->densidadePopulacional = calculaDensidadePopulacional(carta->area, carta->populacao);
 
-    // Início exibição de Resultados
-    printf("Obrigado pela inclusão de dados. Eis os dados inseridos: \n");
-    printf("Codigo Cidade: %s\n", codigoCidade);
-    printf("Nome Cidade: %s\n", nomeCidade);
-    printf("Quantidade de pontos turisticos: %d \n", qtdPontosTuristicos);
-    printf("Area: %f km² \n", area);
-    printf("PIB: %f bilhões de reais \n", pib);
-    printf("PIB per Capita: %f.0 de reais \n", pibPerCapita);
-    printf("Densidade Populacional: %f.2 pessoas/km² \n", densidadePopulacional);
+    printf("\nObrigado pela inclusão de dados. Eis os dados inseridos: \n");
+    printf("Codigo Cidade: %s\n", carta->codigo);
+    printf("Nome Cidade: %s\n", carta->nome);
+    printf("Quantidade de pontos turisticos: %d \n", carta->qtdPontosTuristicos);
+    printf("Area: %.2f km² \n", carta->area);
+    printf("PIB: %.2f de reais \n", carta->pib);
+    printf("PIB per Capita: %.2f de reais \n", carta->pibPerCapita);
+    printf("Densidade Populacional: %f pessoas/km² \n", carta->densidadePopulacional);
+}
 
-    // FIM exibição de Resultados
+int main() {
+    Carta carta;
+
+    exibirInstrucoes();
+    
+    capturarDados(&carta);
+    
+    exibirResultados(&carta);
 
     return 0;
 }
